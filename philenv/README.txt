e258e9026a0fb189ce39ee91f3fc861fc4ffc4d1
Phidra <phidra@pm.me>
2023-07-19 15:01:16 +0200

    update suite à nouvelle installation

--- philenv/README.txt
+++ philenv/README.txt
@@ -1,13 +1,16 @@
-NOTES LORS DU TRANSFERT SUR GITHUB LE 25 JUIN 2023 = comme j'ai fait un peu de cleaning, ce serait sans doute plus simple de tout agréger dans le zshrc, plutôt que de maintenir un projet séparé.
+NOTES LORS DU TRANSFERT SUR GITHUB LE 25 JUIN 2023 = comme j'ai fait un peu de cleaning, ce serait sans doute plus simple d'agréger un gros bloc de code dans le zshrc, plutôt que de maintenir un projet séparé.
 
 ================================================================================
 Pour installer le philenv sur une machine :
 ================================================================================
-    1. Appeler ./release.sh
+    1. Appeler ./release_philenv.sh
     2. En root, copier le répertoire créé dans "/opt"
+
+        sudo cp -R RELEASED_philenv.d_14décembre2013_11h08m30s /opt
+
     3. Le rendre accessible en lecture et exécution à tout le monde, récursivement :
 
-        sudo chmod -R o+rx RELEASED_philenv.d_14décembre2013_11h08m30s
+        sudo chmod -R o+rx /opt/RELEASED_philenv.d_14décembre2013_11h08m30s
 
     4. En créer un lien sous "/opt/philenv.d" :
 
--- philenv/release_philenv.sh
+++ philenv/release_philenv.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
-########################################################################################################################
-#
 # Fichier de release du philenv.
 # Génère un répertoire à copier quelque part (par exemple dans "/opt/philenv.d"), puis à sourcer depuis le bashrc.
-# Produit en sortie le nom du répertoire du philenv.
-#
-########################################################################################################################
 
-relative_script_dir=$(dirname "${0}")
-script_dir=$(readlink -e "${relative_script_dir}")
+set -o errexit
+set -o nounset
+set -o pipefail
+# set -o xtrace
 
-pushd "${script_dir}" &> /dev/null
+this_script_parent="$(realpath "$(dirname "$0")" )"
+
+
+pushd "${this_script_parent}" &> /dev/null
 
 # Copie des sources dans un répertoire released :
 DATE=$(date +'%d%B%Y_%Hh%Mm%Ss')
-released_dirname="${script_dir}/RELEASED_philenv.d_${DATE}"
-cp -R "philenv" "${released_dirname}"
+released_dirname="${this_script_parent}/RELEASED_philenv.d_${DATE}"
+cp -R "src" "${released_dirname}"
 
 # Remplissage du fichier de version et copie :
 GIT_VERSION=$(git show --summary)
@@ -25,15 +25,7 @@ echo "RELEASE DATE = ${DATE}" > "${git_version_file}"
 echo "" >> "${git_version_file}"
 echo "GIT_VERSION = ${GIT_VERSION}" >> "${git_version_file}"
 
-popd "${script_dir}" &> /dev/null
+popd &> /dev/null
 
-# Nom du répertoire produit :
+# On indique le répertoire créé :
 echo "${released_dirname}"
-
-
-
-
-
-########################################################################################################################
-# Fin du fichier.
-########################################################################################################################
