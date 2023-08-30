64cf5ad0c7851bcc5759c3416e9e2366832ef458
Phidra <phidra@pm.me>
2023-08-30 20:19:12 +0200

    Slightly better machine-dependent stuff

--- philenv/src/machine_dependent_stuff.sh
+++ philenv/src/machine_dependent_stuff.sh
@@ -1,27 +1,15 @@
 # Ce fichier a vocation à être différent pour chaque machine sur lequel le philenv est installé.
 # Il contient ce qui est machine-dependent (e.g. le répertoire des projets gits)
 # Il doit être édité manuellement sur chaque machine où le philenv est installé.
+# GUIDELINE : rien dans le reste du philenv ne doit dépendre du contenu de ce fichier.
 
-
-#################################################################################
-# projects related :
-#################################################################################
-
-export projects="/media/DATA/projects"
-export gitprojects="${projects}/github"
 function gogit
 {
-    gogit_base "$gitprojects"
+    gogit_base "/media/DATA/projects/github"
 }
-alias gojour="cd ${projects}/JournauxDeSuivi"
-
-#################################################################################
-# ci-dessous, les autres trucs machine-related :
-#################################################################################
-
-export DATA="/media/DATA"
-alias godata="cd ${DATA}"
+alias gojour="cd /media/DATA/projects/JournauxDeSuivi"
+alias godata="cd /media/DATA"
 alias god="godata"
 
 # À moins qu'on ait ouvert le terminal sur une localisation spéciale (dans ce cas, le pwd n'est pas HOME), on va directement dans DATA :
-[ "$(pwd)" = "${HOME}" ] && cd "${DATA}"
+[ "$(pwd)" = "${HOME}" ] && cd "/media/DATA"
